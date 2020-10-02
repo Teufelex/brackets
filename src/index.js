a3637bf15d97e9d940e970e125f36e55f6a9d4dd
Teufelex <gloomycoke@gmail.com>
2020-10-02 04:18:12 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  for (let i = 0; i < arr.length;) {
+       if (arr[i] == '(' && arr[i+1] == ')') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '{' && arr[i+1] == '}') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '[' && arr[i+1] == ']') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '|' && arr[i+1] == '|') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '1' && arr[i+1] == '2') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '3' && arr[i+1] == '4') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '5' && arr[i+1] == '6') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '7' && arr[i+1] == '7') {
+        arr.splice(i, 2);
+        i = 0;
+      } else if (arr[i] == '8' && arr[i+1] == '8') {
+        arr.splice(i, 2);
+        i = 0;
+      } else {
+          i++;
+      }
+  }
+
+  if (arr.length == 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
